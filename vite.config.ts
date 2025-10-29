b02277fac2d142d977698ec2aed3399ee32fee8c
Sophie Oglesson <seoglesson@gmail.com>
2025-10-29 20:45:35 +0000

    trying to set up deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
+	base: './',
 	plugins: [sveltekit(), mkcert(), devtoolsJson()],
 	test: {
 		expect: { requireAssertions: true },
