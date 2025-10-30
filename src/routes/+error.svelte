a7b5c39d16c87322eee88f7d81d9720283e61a3e
Sophie Oglesson <seoglesson@gmail.com>
2025-10-30 19:51:33 +0000

    fix issues with static generation not working as expected

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+{#if page.status === 404}
+	<h1>Not found</h1>
+	<p><a href="/">Go to /</a></p>
+{:else}
+	<p>
+		Server-side rendering failed with HTTP status
+		{page.status}
+	</p>
+{/if}
+
+<style>
+	h1 {
+		font-weight: 200;
+		font-size: 2rem;
+		margin: 0 0 0.5em 0;
+	}
+</style>
