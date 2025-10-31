228d27d8ca1316013464829b1a66b41adb81a947
Sophie Oglesson <seoglesson@gmail.com>
2025-10-31 15:54:14 +0000

    still trying to fix build

--- svelte.config.js
+++ svelte.config.js
@@ -11,13 +11,10 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	}
 };
 
